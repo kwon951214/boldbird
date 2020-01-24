8d3f97b29a3e19123415d00043a679b9859d4195
GwonGyeongmin <kwon951214@naver.com>
2020-01-25 00:51:34 +0900

    vue4-4

--- ch1/back/app.js
+++ ch1/back/app.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     // res.send('안녕 백앤드');
-    res.status(200).send('안녕 백엔드');
+    res.status(200).send('안녕 GGM');
+});
+
+app.post('/user', (req, res) => {
+    req.body.email;
+    req.body.password;
+    req.body.nickname;
 });
 
 app.listen(3085, () => {
