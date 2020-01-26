0f678e5b71379c52c2714d1d856abaa1daf1899c
GwonGyeongmin <kwon951214@naver.com>
2020-01-26 23:46:21 +0900

    vue4-5

--- ch1/back/app.js
+++ ch1/back/app.js
@@ -1,19 +1,32 @@
 const express = require('express');
-
+const db = require('./models');
 const app = express();
 
+db.sequelize.sync();
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     // res.send('안녕 백앤드');
     res.status(200).send('안녕 GGM');
 });
 
-app.post('/user', (req, res) => {
-    req.body.email;
-    req.body.password;
-    req.body.nickname;
+app.post('/user', async (req, res, next) => {
+    try {
+        const newUser = await db.User.create({
+            where: {
+                email: req.body.email,
+                password: req.body.password,
+                nickname: req.body.nickname
+            }
+        });//HTTP STATUS CODE
+        res.status(201).json(newUser); //문자열일땐 json말고 send
+    } catch (e) {
+        console.log(error);
+        next(error);
+    }
+
 });
 
 app.listen(3085, () => {
--- ch1/back/models/index.js
+++ ch1/back/models/index.js
@@ -4,8 +4,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+db.User = require('./user')(sequelize, Sequelize);
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
